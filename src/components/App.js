f1f8af076feea0d945b971f2b3506f5d9d833313
Tatiana Uspenskaia <tanyauspenskaya@gmail.com>
2019-01-17 21:23:03 -0500

    setup and structure

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../stylesheets/main.scss';
+import React from 'react';
+import Header from './Header';
+
+class App extends React.Component {
+  render(){
+    return(
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return(
+    <div className="header">
+      <h1 className="header__title">Toronto Waste Lookup</h1>
+    </div>
+  );
+};
+
+export default Header;
