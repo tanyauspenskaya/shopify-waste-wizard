e2c4e3d5b7e14ca5d7185a590ccaad8ea6c16590
Tatiana Uspenskaia <tanyauspenskaya@gmail.com>
2019-01-20 14:13:52 -0500

    Updates

---
+++ precache-manifest.23828bf4a3a64fda604e55925243524a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0415de8e48e6315c883e92bc4eb23fb4",
+    "url": "/shopify-waste-wizard/static/media/icons.0415de8e.svg"
+  },
+  {
+    "revision": "c7e3df940dd934d73b99",
+    "url": "/shopify-waste-wizard/static/js/runtime~main.c7e3df94.js"
+  },
+  {
+    "revision": "e7dd82518149b7045214",
+    "url": "/shopify-waste-wizard/static/js/main.e7dd8251.chunk.js"
+  },
+  {
+    "revision": "e354f00db4e948125a90",
+    "url": "/shopify-waste-wizard/static/js/1.e354f00d.chunk.js"
+  },
+  {
+    "revision": "e7dd82518149b7045214",
+    "url": "/shopify-waste-wizard/static/css/main.bd46a756.chunk.css"
+  },
+  {
+    "revision": "7d827a52dfafe4fdd4387db033685ab0",
+    "url": "/shopify-waste-wizard/index.html"
+  }
+];
